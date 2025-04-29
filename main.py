bd3602fd93a0f61a3251940c52e4991ab7026a8f
HelloCheck <helloyourscrewed2@gmail.com>
2025-04-29 17:20:38 +0800

    Every file into 1

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pygame, time
 import random
-import re
 from sys import exit
-from items import item_data
-from image import *
 
 pygame.init()
 
@@ -22,7 +19,31 @@ fish_ico = pygame.image.load('image/icon.ico')
 pygame.display.set_icon(fish_ico)
 display = pygame.Surface(res)
 
-# text
+###########################################################################
+
+# Images
+BASE_PATH = 'image/'
+
+# Loads images and removes any black color
+def load_image(path):
+    image = pygame.image.load(BASE_PATH + path).convert()
+    image.set_colorkey((0, 0, 0))
+    return image
+
+# Loads images that have transparency
+def load_image_alpha(path):
+    image = pygame.image.load(BASE_PATH + path).convert_alpha()
+    return image
+
+# Makes a Surface and colors it in
+def draw_surface(size, color):
+    surf = pygame.Surface(size, pygame.SRCALPHA)
+    surf.fill(color)
+    return surf
+
+###########################################################################
+
+# Text
 # Creates text
 def load_text(text = 'Hi HelloCheck!', size = 16, color = (255, 255, 255)):
     font = pygame.font.Font('text/m6x11.ttf', size)
@@ -36,12 +57,16 @@ def new_line(text, length):
     text = ''.join(lines)
     return text
 
+###########################################################################
+
 # Returns the numbers that centers a surface on another Surface
 def cen_surface(surfa, display = res):
     cen = [(display[0] // 2) - (surfa.get_width() // 2), (display[1] // 2) - (surfa.get_height() // 2)]
     return cen
 
 text = load_text('hello', 16, (255, 255, 255))
+
+###########################################################################
 
 # Assets
 # The sky and the clouds are from https://free-game-assets.itch.io/free-sky-with-clouds-background-pixel-art-set
@@ -60,6 +85,81 @@ assets = {
     'inside': load_image('House/Inside.png'),
     'outside': load_image('House/Outside.png')
 }
+
+###########################################################################
+
+# Item Data
+class Item:
+    def __init__(self, name, value, count, icon, description, book):
+        self.name = name
+        self.value = value
+        self.count = count
+        self.icon = icon
+        self.desc = description
+        self.book = book
+
+    # Fish images that are inside the BookFish folder is from 
+    # https://oceana.org/ocean-fishes/ turned into pixelated images
+    # Also data
+
+    # Blue Bandedgoby, California Grunion, Chilean Common Hake, Chilean Jack Mackerel, Blue Marlin, 
+    # Atlantic Trumpetfish, Bluehead Wrasse fish icons are drawn by me
+    # The rest of the fish icons are taken off of https://www.shutterstock.com/ royalty-free images and turned into pixelated images
+
+    # Fish information is from ChatGPT
+
+item_data = {
+    '1': Item('Atlantic Bluefin Tuna', 100, 0, 'BluefinTuna.png',
+              'Atlantic Bluefin Tuna are the largest tunas, growing up to 13 feet long and weighing as much as 2,000 pounds. They can live up to 40 years and are known for their incredible speed and deep diving abilities.',
+              'AtlanticBluefinTuna.png'),
+    '2': Item('Atlantic Cod', 100, 0, 'Cod.png',
+              'Atlantic Cod can live more than 20 years and grow up to 51 inches and 77 pounds. They are top predators in the bottom ocean community, feeding on a variety of invertebrates and fish.',
+              'AtlanticCod.png'),
+    '3': Item('Atlantic Goliath Grouper', 100, 0, 'Grouper.png',
+              'The Atlantic Goliath Grouper is the largest species of grouper in the Atlantic Ocean, reaching up to 8 feet in length and over 800 pounds in weight.',
+              'AtlanticGoliathGrouper.png'),
+    '4': Item('Atlantic Salmon', 100, 0, 'Salmon.png',
+              'Atlantic Salmon are anadromous, hatching in freshwater and migrating to the ocean to grow. They return to freshwater to spawn, and larger individuals have more eggs, aiding population sustainability.',
+              'AtlanticSalmon.png'),
+    '5': Item('Atlantic Trumpetfish', 100, 0, 'Trumpetfish.png',
+              'The Atlantic Trumpetfish uses its large snout and triangle-shaped head to create suction, allowing it to suck in prey like small fishes and mobile invertebrates.',
+              'AtlanticTrumpetfish.png'),
+    '6': Item('Atlantic Wolffish', 100, 0, 'Wolffish.png',
+              'Atlantic Wolffish produce natural antifreeze to keep their blood flowing in cold waters. They are important predators, controlling populations of green crabs and sea urchins.',
+              'AtlanticWolffish.png'),
+    '7': Item('Banded Butterflyfish', 100, 0, 'Butterflyfish.png',
+              'Banded Butterflyfish reproduce through broadcast spawning, where a female releases eggs and a male releases sperm into the water column above the reef simultaneously.',
+              'BandedButterflyfish.png'),
+    '8': Item('Beluga Sturgeon', 100, 0, 'Sturgeon.png',
+              'The Beluga Sturgeon is the largest living freshwater fish, capable of growing up to 20 feet in length and weighing over 3,000 pounds.',
+              'BelugaSturgeon.png'),
+    '9': Item('Blue Marlin', 100, 0, 'Marlin.png',
+              'Blue Marlins are among the largest and fastest fish, reaching up to 14 feet in length and weighing nearly 2,000 pounds. They are known for their spear-shaped upper jaws.',
+              'BlueMarlin.png'),
+    '10': Item('Blue Tang', 100, 0, 'Tang.png',
+               'Blue Tangs can raise a pair of razor-sharp, venomous spines on either side of their tails when threatened, using them to deter predators.',
+               'BlueTang.png'),
+    '11': Item('Bluebanded Goby', 100, 0, 'BluebandedGoby.png',
+               'Bluebanded Gobies are microcarnivores that eat small crustaceans on the reef surface and from the water column directly above the reef surface.',
+               'BluebandedGoby.png'),
+    '12': Item('Bluehead Wrasse', 100, 0, 'Wrasse.png',
+               'Bluehead Wrasses are generalist foragers, eating a variety of prey including small invertebrates and crustaceans on the reef surface.',
+               'BlueheadWrasse.png'),
+    '13': Item('California Grunion', 100, 0, 'Grunion.png',
+               'California Grunions are slender, silver fish measuring an average of 5 to 6 inches long. They are known for their unique spawning behavior on sandy beaches.',
+               'CaliforniaGrunion.png'),
+    '14': Item('Chilean Common Hake', 100, 0, 'Hake.png',
+               'The Chilean Common Hake is a merluccid hake found along the coast of South America, from Peru to the Chilean coasts north to the Chiloé Archipelago.',
+               'ChileanCommonHake.png'),
+    '15': Item('Chilean Jack Mackerel', 100, 0, 'Mackerel.png',
+               'Chilean Jack Mackerels are commonly 45 cm long, though they can grow to 70 cm. They have elongated and laterally compressed bodies.',
+               'ChileanJackMackerel.png'),
+    '16': Item('Chinook Salmon', 100, 0, 'ChinookSalmon.png',
+               'Chinook Salmon, also known as King Salmon, are the largest species of Pacific salmon, known for their rich flavor and high oil content.',
+               'ChinookSalmon.png'),
+}
+
+###########################################################################
 
 # Animation Handler
 # Code from Coding With Russ on youtube
@@ -92,7 +192,9 @@ for animation in animation_steps:
         step_counter += 1
     animation_list.append(temp_list)
 
-# player
+###########################################################################
+
+# Player
 class Entity():
     def __init__(self, pos, animations, frame, reverse, action):
         self.pos = list(pos)
@@ -117,7 +219,9 @@ player = Entity([240, 100], animation_list, 0, False, 0)
 
 movement = [0, 0]
 
-# inventory
+###########################################################################
+
+# Inventory
 class Inventory():
     def __init__(self, cols, pos):
         self.items = {}
@@ -160,6 +264,8 @@ class Inventory():
 inv_open = False
 inv = Inventory(8, cen_surface(pygame.Surface((480 // 1.2, 270 // 1.2)), res))
 
+###########################################################################
+
 # Index
 book = assets['book']
 # Scales the book by 2
@@ -186,6 +292,8 @@ class Index():
 idx = Index()
 page = 0
 op = True
+
+###########################################################################
 
 # Fishing
 class Fish():
@@ -220,6 +328,8 @@ cooldown = 0
 
 fish = Fish(50, center, 90)
 
+###########################################################################
+
 # Background
 ocean_r = assets['ocean'].get_rect(bottomleft = (0, 270))
 dock_r = assets['dock'].get_rect(topleft = (64, 125))
@@ -231,7 +341,7 @@ cloud2 = assets['cloud2'].get_rect(bottomleft = (ocean_r.left, ocean_r.top))
 exterior_r = assets['outside'].get_rect(bottomleft = (dock_r.centerx - (assets['outside'].get_width() // 2), dock_r.top))
 interior_r = assets['inside'].get_rect(bottomleft = (dock_r.centerx - (assets['inside'].get_width() // 2), dock_r.top))
 
-# main loop
+# Main loop
 while True:
     # Updates animation
     current_time = pygame.time.get_ticks()
