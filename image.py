17b3153f5931b8592821fd9a8c3850afbd57bfc3
HelloCheck <helloyourscrewed2@gmail.com>
2025-04-29 17:06:19 +0800

    stuff

--- image.py
+++ image.py
@@ -7,11 +7,6 @@ def load_image(path):
     image.set_colorkey((0, 0, 0))
     return image
 
-def rect_load_image(path):
-    image = load_image(path)
-    image_rect = image.get_rect()
-    return image, image_rect
-
 def load_image_alpha(path):
     image = pygame.image.load(BASE_PATH + path).convert_alpha()
     return image
